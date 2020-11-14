7341b0477a95e4f2b35117bc5e2f254e412ff206
Hamo-Hamo <70852832+Hamo-Hamo@users.noreply.github.com>
2020-11-14 13:15:13 +0900

    PID制御ライブラリ

---
+++ NuKe_src/NK_PID.cpp
@@ -0,0 +1,35 @@
+#include "NK_PID.hpp"
+
+Nk_pid::Nk_pid(double kp,double ki,double kd,double freq){
+    Kp=kp;
+    Ki=ki;
+    Kd=kd;
+    Freq=freq;
+    err[0]=0;
+    }
+void Nk_pid::SetParam(double Value,double Target){
+  err[1]=Target-Value;
+  integral+=(err[1]+err[0])/2.0*Freq;
+  G_P=err[1];
+  G_I=integral;
+  G_D=(err[1]-err[0])/Freq;
+  err[0]=err[1];
+  res+=(Kp*G_P)+(Ki*G_I)+(Kd*G_D);
+  res=(res>Max_P)?Max_P:(res<Max_M)?Max_M:res;
+}
+double Nk_pid::GetGain(double xvalue){
+  if(res<0){
+    rtn=(res<Max_M)?Max_M:(res>Min_M)?0:res;
+      }
+  else {
+    rtn=(res>Max_P)?Max_P:(res<Min_P)?0:res;
+      }
+  return (rtn/xvalue);
+}
+void Nk_pid::SetLimit(double max_p,double min_p,double max_m,double min_m){
+  Max_P = max_p*255.0;
+  Min_P = min_p*255.0;
+  Max_M = max_m*255.0;
+  Min_M = min_m*255.0;
+}
+Nk_pid::~Nk_pid(){}
---
+++ NuKe_src/NK_PID.hpp
@@ -0,0 +1,33 @@
+#ifndef NK_PID
+#define NK_PID
+/*
+PIDのライブラリです、動作時間の固定の処理は含まれていないので各自でおねがいします。
+ */
+class Nk_pid{
+public:
+  Nk_pid(double kp,double ki,double kd,double freq);//初期設定
+  ~Nk_pid();
+  void SetParam(double Value,double Target);//現在値、目標値入力
+  void SetLimit(double max_p,double min_p,double max_m,double min_m);//正負回転の上下限設定
+  double GetGain(double xvalue);//255~-255がデフォルト 割合設定
+private:
+  double Max_P;
+  double Min_P;
+  double Max_M;
+  double Min_M;
+  double Kp;
+  double Ki;
+  double Kd;
+  double Freq;
+  double G_P;
+  double G_I;
+  double G_D;
+  double integral;
+  double value;
+  double target;
+  double err[2];
+  double res;
+  double rtn;
+};
+
+#endif
