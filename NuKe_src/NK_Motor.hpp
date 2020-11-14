29152ec400f39cb45fecbef258d38e77d1ee1ab6
Hamo-Hamo <70852832+Hamo-Hamo@users.noreply.github.com>
2020-11-14 13:14:36 +0900

    モーターのライブラリmbed用

---
+++ NuKe_src/NK_Motor.hpp
@@ -0,0 +1,22 @@
+#ifndef NK_Motor
+#define NK_Motor
+/*
+PwmOut書くのだるいと思ったので作りました。
+ */
+#include "mbed.h"
+
+class Nk_motor{
+public:
+  Nk_motor(PinName plus,PinName minus,int period);//モーターのピンとデータを送る周波数を設定
+  ~Nk_motor();
+  void SetLimit(double max,double min);//モーターの正負の最大PWM設定
+  void pwmout(double pwm);//pwm出力
+private:
+  double Max;
+  double Min;
+  PwmOut *Plus;
+  PwmOut *Minus;
+  double Pwm;
+};
+
+#endif
